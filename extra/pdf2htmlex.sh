6bfd8c5756c6bd0377371a919bd0e04bf65e67ed
Charlie DeTar <cfd@media.mit.edu>
2014-10-07 12:09:44 -0600

    Add alias for pdf2htmlEX

--- extra/pdf2htmlex.sh
+++ extra/pdf2htmlex.sh
@@ -8,3 +8,9 @@ echo "-----> Installing pdf2htmlex."
 for url in "$FONTFORGE_URL" "$POPPLER_URL" "$PDF2HTMLEX_URL" ; do
   curl -sS $url -o - | tar -zxf - -C $COMPILE_DIR --strip 2
 done
+# Add an alias to set "data-dir" for pdf2htmlEX every time we call it, since
+# our pre-compiled binary puts the default data-dir somewhere random.
+mkdir -p "$APP_CHECKOUT_DIR"/.profile.d
+cat > "$APP_CHECKOUT_DIR"/.profile.d/pdf2htmlex-alias.sh <<EOF
+    alias pdf2htmlEX="pdf2htmlEX --data-dir \$HOME/$COMPILE_DIR_SUFFIX/share/pdf2htmlEX" 
+EOF
