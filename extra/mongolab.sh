14af8fa61991af83297002fd7b2905d999d4e10a
Nicolás López <nicolaslopezj@me.com>
2014-12-17 01:48:16 -0300

    detect

--- extra/mongolab.sh
+++ extra/mongolab.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-# Add an alias of MONGOHQ_URL to MONGO_URL.
-#echo "-----> Adding MONGOLAB_URI -> MONGO_URL env"
-#cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo.sh <<EOF
+# Add an alias of MONGOLAB_URI to MONGO_URL.
+echo "-----> Adding MONGOLAB_URI -> MONGO_URL env"
+cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo.sh <<EOF
   #!/bin/sh
-#  export MONGO_URL=\${MONGO_URL:-\$MONGOLAB_URI}
-##EOF
-
+  export MONGO_URL=\${MONGO_URL:-\$MONGOLAB_URI}
+EOF
