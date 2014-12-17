ae9ea150418e22b9c2f92a67226c0823833a0e77
Charlie DeTar <cfd@media.mit.edu>
2014-12-17 07:19:06 -0700

    Rename mongolab extra, echo Node.js in detect

--- extra/mongolab.sh
+++ extra/mongolab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Add an alias of MONGOHQ_URL to MONGO_URL.
-echo "-----> Adding MONGOHQ_URL -> MONGO_URL env"
+echo "-----> Adding MONGOLAB_URI -> MONGO_URL env"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo.sh <<EOF
   #!/bin/sh
   export MONGO_URL=\${MONGO_URL:-\$MONGOLAB_URI}
