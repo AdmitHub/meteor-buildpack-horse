8b8cced75deb933653f02b83b860574fa39a97d2
Charlie DeTar <cfd@media.mit.edu>
2016-04-16 11:08:53 -0600

    Don't remove platforms; add --server-only

--- extra/root_url.sh
+++ extra/root_url.sh
@@ -6,6 +6,6 @@ echo "-----> Adding profile script to resolve ROOT_URL from heroku app name"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/root_url.sh <<EOF
   #!/bin/bash
   if [ -z \$ROOT_URL ] ; then
-    export ROOT_URL="https://\${HEROKU_APP_NAME}.herokuapp.com"
+    export ROOT_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
   fi
 EOF
