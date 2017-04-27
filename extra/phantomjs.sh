f786977ecd69c77eabe8db595fe2ff5ccc979f9b
Gabriel Engel <gabriel.engel@gmail.com>
2017-04-27 00:59:14 -0300

    VERSION -> PHANTOM_VERSION

--- extra/phantomjs.sh
+++ extra/phantomjs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-VERSION=1.9.20
-PHANTOM_URL="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$VERSION-linux-x86_64.tar.bz2"
+PHANTOM_VERSION=1.9.20
+PHANTOM_URL="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOM_VERSION-linux-x86_64.tar.bz2"
 
 echo "-----> Installing phantomjs."
 # Just extract the one file (the binary) and not all the examples/readme's/etc.
-FILE="phantomjs-$VERSION-linux-x86_64/bin/phantomjs"
+FILE="phantomjs-$PHANTOM_VERSION-linux-x86_64/bin/phantomjs"
 curl -L -sS $PHANTOM_URL -o - | tar -jxf - -C $COMPILE_DIR $FILE --strip 1
