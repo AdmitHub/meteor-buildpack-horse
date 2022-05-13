a4dacd428eb7e548c9bb3d2ad863f17e469a5477
ToyboxZach <zach@make.toys>
2022-05-12 17:06:15 -0700

    fix

--- lib/start.sh
+++ lib/start.sh
@@ -16,4 +16,4 @@ if [[ -n "$HEROKU_METRICS_URL" ]] && [[ "${DYNO}" != run\.* ]] && [[ -z "$HEROKU
 fi
 
 echo "RUNNING with" $NODEJS_PARAMS
-.heroku/node/bin/node $NODEJS_PARAMS .meteor/heroku_build/app/main.js
+ $HOME/.meteor/heroku_build/node/bin/node $NODEJS_PARAMS .meteor/heroku_build/app/main.js
